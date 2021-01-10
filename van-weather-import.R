43850f70e57fc4929f76df4959396cc5272aae65
jyuill <john@fig4.com>
2021-01-09 18:33:05 -0800

    major overhaul, switching weather stations to vancouver intl for better reliability/completeness. streamlined system to work with either current yr or multiple yrs in one process.

--- van-weather-import.R
+++ van-weather-import.R
@@ -10,7 +10,11 @@ library(googledrive)
 ### FULL MANUAL VERSION - skip to SHORTCUT section below for DIRECT DOWNLOAD ####
 ## weather data from: (skip 4a below to go straight to Van harbour download page)
 ## 1. search: http://climate.weather.gc.ca/historical_data/search_historic_data_e.html
-## 2. specify station as containing 'vancouver harbour'
+## 2. specify desired station:
+###   - originally used 'vancouver harbour' but issues cropped up
+###   - can enter 'vancouver' in form to find stations -> many attractive-looking options BUT
+###   - only VANCOUVER INTL A has reliable data (preceded by VANCOUVER INT'L A in 2013)
+###   - info in 'input/1-van-weather-stations.csv'
 ## 3. select 'daily', select 'year'
 ## 4. 'Go' goes to a page to access results for year
 ## 4a. SHORTCUT TO VANCOUVER HARBOUR FOR GIVEN YEAR
@@ -20,17 +24,48 @@ library(googledrive)
 ## 5. Download as csv
 ## 6. Save with 'van-hrbr-weather-' prepended (in GDrive > Data)
 
-### SHORTCUT VERSION -> DIRECT DOWNLOAD ####
-## 1. Generate URL that matches Vancouver Harbour download page for specified year:
-## year for data
-yr <- 2020
-## data url
-data_url <- paste0("http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=888&Year=",yr,"&Month=12&Day=1&timeframe=2&submit=Download+Data")
-## destination to save file SHOULD CONVERT TO USING GDRIVE ID
-file_save_url <- paste0("~/../Google Drive/01 MyProHQ/Raw-Data/van-hrbr-weather-eng-daily-0101",yr,"-1231",yr,".csv")
-## download and save file
+## START DATA COLLECTION /////////////////////////////////////////////////////
+## SET PARAMETERS ####
+## STATION 
+## need station id; get from saved file
+stn_info <- read_csv('input/1-van-weather-stations.csv')
+stn <- "VANCOUVER INTL A"
+## get stn id from name
+stn_id <- stn_info %>% filter(name==stn) %>% select(id)
+## clean stn name for inclusion in file save name
+stn_clean <- str_replace_all(stn," ","_")
+stn_clean <- str_replace_all(stn_clean,"'","")
+## YEAR
+## year for data -> date is set to 12-31 of selected yr; will collect whatever is available
+## if current yr (or any single yr) set start and end to current yr
+yr_start <- 2013
+yr_end <- 2020
+
+
+## DATA COLLECTION LOOP #### 
+## empty data frame to hold data from each loop cycle
+vw_all_yrs <- data.frame()
+yr_data <- yr_start
+
+## LOOP ####
+## - loop through each yr, get all available data for SPECIFIED YR, one yr at a time
+## - should probably be WHILE but this works ;)
+for(y in 1:(yr_end-yr_start+1)){
+## 1. Generate URL that identifies station id and date range:
+## data url for fetching
+data_url <- paste0("http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=",
+stn_id[[1]],
+"&Year=",
+yr_data,"&Month=12&Day=31&timeframe=2&submit=Download+Data")
+
+## destination to SAVE file
+## save URL
+file_save_url <- paste0("input/",
+                       stn_clean,"weather-eng-daily-0101",yr_data,"-1231",yr_data,".csv")
+## DOWNLOAD and SAVE data file ####
 download.file(url=data_url, destfile = file_save_url)
-## import file into R - skip heading rows
+
+## import file back into R - skip heading rows
 ## FILE STRUCTURE CHG OCT 2019
 #vw.new <- read_csv(file_save_url, skip=25)
 ## NEW DATA STRUCTURE DOESN'T NEED SKIP
@@ -42,6 +77,20 @@ vw.new$Day <- as.numeric(vw.new$Day)
 summary(vw.new)
 str(vw.new)
 head(vw.new)
+
+## CHECK NA ####
+## check NAs for key cols ()
+any(is.na(vw.new)[,c(5,10,12,14,20,24)])
+if(any(is.na(vw.new)[,c(5,10,12,14,20,24)])){
+  print("missing max temp")
+  print(vw.new[which(is.na(vw.new[10])),c(5,10,12,14,20,24)])
+  print("missing min temp")
+  print(vw.new[which(is.na(vw.new[12])),c(5,10,12,14,20,24)])
+  print("missing total precip")
+  print(vw.new[which(is.na(vw.new[24])),c(5,10,12,14,20,24)])
+} else {
+  print("no key data missing")
+}
 
 ## CHARTS: check imported data ####
 ## bar chart of max temp (colnames changed to be easier to work with)
@@ -58,6 +107,7 @@ ggplot(vw.new2, aes(x=date, y=`Total Precip (mm)`))+geom_bar(stat='identity')
 
 ## most recent week
 vw.latest.wk <- vw.new2 %>% filter(date>Sys.Date()-8 & date<Sys.Date())
+vw.latest.wk <- tail(vw.new2, n=7)
 ## bar chart max temp
 ggplot(vw.latest.wk, aes_string(x="date", y="maxtemp"))+
   geom_bar(stat='identity')+
@@ -95,8 +145,9 @@ vw.new.sel <- vw.new.sel %>% rename(
   Mean.Temp=meantemp,
   Total.Precip=`Total Precip (mm)`
 )
-## drop empty rows at end
-vw.new.sel.last <- vw.new.sel %>% filter(!is.na(Max.Temp), !is.na(Min.Temp), !is.na(Mean.Temp)) %>%
+## drop empty rows at end <- probably better way to determine this
+## get latest date where key fields are not all NA
+vw.new.sel.last <- vw.new.sel %>% filter(!is.na(Max.Temp), !is.na(Min.Temp), !is.na(Mean.Temp), !is.na(Total.Precip)) %>%
   filter(Date==max(Date))
 vw.new.sel <- vw.new.sel %>% filter(Date<=vw.new.sel.last[[1]])
 ## apply season values
@@ -106,25 +157,40 @@ vw.new.sel <- left_join(seasons.mth, vw.new.sel, by='Month')
 vw.new.sel <- vw.new.sel %>% mutate(
   Season.Yr=ifelse(Month==12,Year+1,Year))
 
+## SAVE yr_data from loop ####
+write_csv(vw.new.sel, paste0("output/",stn_clean,"-weather-",yr_data,".csv"))
+
+## BIND with prev data in loop####
+vw_all_yrs <- bind_rows(vw_all_yrs, vw.new.sel)
+
+## indicate yr completed for progress/debugging
+cat(paste0("Collected: ", yr_data,"\n"))
+
+## add to yr var for next yr 
+yr_data <- yr_data+1
+
+} ## END DATA COLLECTION LOOP ####
+## /////////////////////////////////////////////////
+
+## SAVE all yrs data
+write_csv(vw_all_yrs, paste0("output/",stn_clean,"-weather-",min(vw_all_yrs$Year),"-",max(vw_all_yrs$Year),".csv"))
+
 ## UPDATE EXISTING DATA WITH NEW ####
-## Load existing data
-vw.exist <- read_csv("input/van-hrbr-weather.csv")
-# vw.exist <- vw.exist %>% mutate(
-#   Season.Yr=ifelse(Month==12,Year+1,Year))
-#vw.exist.ws <- vw.exist %>% filter(Month==1, Season=='Winter')
-#vw.exist <- vw.exist %>% select(1,2,Season.Yr,3:9)
-## determine most recent data for existing data
-## this looks circular: determine most recent date then filter for
-## everything before most recent date? maybe some cleanup?
-vw.recent <- vw.exist %>% filter(!is.na(Max.Temp)) %>% 
-  filter(Date==max(Date))
-vw.exist <- vw.exist %>% filter(Date<=vw.recent$Date[1])
+## if first time collecting data for stn, save file
+## otherwise, append new data to existing file
+if(!file.exists(paste0("output/",stn_clean,"-weather.csv"))){
+  write_csv(vw_all_yrs, paste0("output/",stn_clean,"-weather.csv"))
+  vw.all <- vw_all_yrs
+} else {
+  ## Load existing data ####
+  vw.exist <- read_csv(paste0("output/",stn_clean,"-weather.csv"))
+  ## compare max date existing with max date new
+  vw_latest <- vw_all_yrs %>% filter(Date>max(vw.exist$Date))
+  ## ADD Latest to existing ####
+  vw.all <- bind_rows(vw.exist, vw_latest)
+}
 
-## SELECT dates past existing from new data set####
-vw.latest <- vw.new.sel %>% filter(Date>vw.recent$Date[1])
-
-## BIND NEW to existing ####
-vw.all <- bind_rows(vw.exist, vw.latest)
+## check dates
 summary(vw.all$Date)
 
 ## test for duplicated dates
@@ -136,12 +202,12 @@ table(duplicated(vw.all$Date))
 #   facet_grid(Year~.)
 
 ## SAVE ####
-write.csv(vw.all, "input/van-hrbr-weather.csv", row.names = FALSE)
-#write_csv(vw.all, paste0("input/van-hrbr-weather_",Sys.Date(),".csv"))
-write_csv(vw.all, paste0("input/van-hrbr-weather_",max(vw.all$Date),".csv"))
+write_csv(vw.all, paste0("input/",stn_clean,"-weather.csv"))
+write_csv(vw.all, paste0("input/",stn_clean,max(vw.all$Date),"-weather.csv"))
 
-## END REG. UPDATE ###################################
+## END UPDATE ###################################
 
+## MULTI-YEAR ####
 ###\\\\\\\\\\\\\\\\\\\\ NOT NEEDED FOR REGULAR USE
 ## X LOOP FOR MULTIPLE YEARS OF HISTORICAL DATA ####
 # yr_start <- 1988
