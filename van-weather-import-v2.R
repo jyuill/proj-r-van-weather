32afb2b93739926422f3b7ada66b6a12bc141195
John Yuill <john@fig4.com>
2021-07-02 19:23:30 -0700

    update data, tweaks to markdown

--- van-weather-import-v2.R
+++ van-weather-import-v2.R
@@ -1,7 +1,13 @@
 ## UPDATE WEATHER DATA FROM SOURCE
 ## climate.weather.gc.ca/historical-data -> details below
 ## SIMPLIFIED VERSION FOR QUICK UPDATING DURING CURRENT YR - van-weather-import.R has full version
-## check summary on Date (code below) to ensure most recent data obtained
+## 1. Fetch existing data for date reference and appending later
+## 2. Fetch new data, clean, filter for recent dates not in existing data
+##    - based on specified weather station
+## 3. Check for Total Precip NAs and if exist, replace with alternate station
+## 4. Append new to existing data 
+## 5. Save
+## check summary on Date (code below) to confirm most recent data obtained
 
 library(tidyverse)
 library(lubridate)
@@ -34,14 +40,12 @@ fStn_data <- function(stn, yr_data){
   vw.new_stn <- read_csv(file_save_url)
   vw.new_stn$Month <- as.numeric(vw.new_stn$Month)
   vw.new_stn$Day <- as.numeric(vw.new_stn$Day)
-  vw.new_stn$Stn <- stn
+  vw.new_stn$Station <- stn
   return(vw.new_stn)
 }
 
-
-
 ## clean up station data
-fVw_stn_clean <- function(vw.new_stn){
+fVw_stn_clean <- function(vw.new_stn, maxD){
   ## first RENAME - using col references because awkward naming/symbols
   vw.new2 <- vw.new_stn %>%
     rename(Date=`Date/Time`)
@@ -49,115 +53,89 @@ fVw_stn_clean <- function(vw.new_stn){
   colnames(vw.new2)[12] <- "Min.Temp"
   colnames(vw.new2)[14] <- "Mean.Temp"
   colnames(vw.new2)[24] <- "Total.Precip"
-  ## SELECT
-  vw.new.sel <- vw.new2 %>% select(Stn, Date, Year, Month, Day,
+  ## SELECT relevant rows
+  vw.new.sel <- vw.new2 %>% select(Date, Year, Month, Day,
                                    Max.Temp, Min.Temp, Mean.Temp,
-                                   Total.Precip)
+                                   Total.Precip, Station)
+  
+  ## apply season values
+  seasons.mth <- read_csv('input/seasons.csv')
+  vw.new.sel <- left_join(seasons.mth, vw.new.sel, by='Month')
+  ## set season yr: Dec falls into Winter of following yr
+  vw.new.sel <- vw.new.sel %>% mutate(
+    Season.Yr=ifelse(Month==12,Year+1,Year))
+  
   ## drop empty rows after most recent date
+  ## NEXT!!! -> setup to filter by AFTER existing data BEFORE today
   vw.new.sel.backup <- vw.new.sel
-  vw.new.sel <- vw.new.sel %>% filter(Date<=Sys.Date()-1)
+  vw.new.sel <- vw.new.sel %>% filter(Date>maxD & Date<=Sys.Date()-1)
+  ## Reorder cols to more intuitive
+  vw.new.sel <- vw.new.sel %>% select(Date, Year, Month, Day, Season, Season.Yr,
+                                      Max.Temp,
+                                      Min.Temp,
+                                      Mean.Temp,
+                                      Total.Precip,
+                                      Station)
   ## return
   return(vw.new.sel)
 }
 
 ## START DATA COLLECTION ####
+## Get existing data to determine what date to start from
+van_exist <- read_csv('output/van-weather.csv')
+van_exist_maxD <- max(van_exist$Date)
 
 ## Get data for selected station - using function above
-stn <- 'VANCOUVER HARBOUR CS'
+stn <- 'VANCOUVER INTL A' ## currently mostly reliable station
+stn_alt <- 'VANCOUVER HARBOUR CS' ## 2nd most reliable - can be used if precip data missing
 yr_data <- year(Sys.Date())
 ## run data function
 vw.new_stn <- fStn_data(stn, yr_data)
 ## run cleaning function
-vw.new_stn_clean <- fVw_stn_clean(vw.new_stn)
+vw.new_stn_clean <- fVw_stn_clean(vw.new_stn, van_exist_maxD)
 
 ## NAs ####
 ## if there are NA check for other station
-## check NAs for key cols ()
-any(is.na(vw.new_stn_clean)[,c('Max.Temp', 'Min.Temp', 'Mean.Temp', 'Total.Precip')])
-if(any(is.na(vw.new_stn_clean)[,c('Max.Temp', 'Min.Temp', 'Mean.Temp', 'Total.Precip')])){
-  vw.new_stn_clean[!complete.cases(vw.new_stn_clean),]
+## check NAs for precip cols - focus on precip because missing data affects total; 
+## - missing temperature data not as significant (probably not significantly affect averages)
+any(is.na(vw.new_stn_clean)[,c('Total.Precip')])
+## if you want to see all incomplete cases
+#vw.new_stn_clean[!complete.cases(vw.new_stn_clean),]
+## SUB IN MISSING PRECIP ####
+if(any(is.na(vw.new_stn_clean)[,c('Total.Precip')])){
+  vw.new_stn_clean[is.na(vw.new_stn_clean$Total.Precip),]
+  ## get alternate data to hopefully fill-in precip as approx
+  vw.new_stn_alt <- fStn_data(stn_alt, yr_data)
+  vw.new_stn_alt_clean <- fVw_stn_clean(vw.new_stn_alt, van_exist_maxD)
+  ## substitute missing precip data from other station
+  ## create loop based on rows with missing precip data
+  na_rows <- which(is.na(vw.new_stn_clean$Total.Precip))
+  for(r in 1:length(na_rows)){
+    r_date <- vw.new_stn_clean$Date[na_rows[r]]
+    r_sub <- vw.new_stn_alt_clean %>% filter(Date==r_date) %>% select(Total.Precip, Station)
+    vw.new_stn_clean$Total.Precip[na_rows[r]] <- r_sub[[1]]
+    vw.new_stn_clean$Station[na_rows[r]] <- r_sub[[2]]
+  }
 } else {
-  print("no key data missing")
+  print("no precipitation data missing")
 }
 
-## old ////// ####
-## import file back into R - skip heading rows
-vw.new <- read_csv(file_save_url)
-vw.new$Month <- as.numeric(vw.new$Month)
-vw.new$Day <- as.numeric(vw.new$Day)
-
-## CLEAN-UP: SELECT columns of interest ####
-## first RENAME - using col references because awkward naming/symbols
-vw.new2 <- vw.new %>%
-  rename(Date=`Date/Time`)
-colnames(vw.new2)[10] <- "Max.Temp"
-colnames(vw.new2)[12] <- "Min.Temp"
-colnames(vw.new2)[14] <- "Mean.Temp"
-colnames(vw.new2)[24] <- "Total.Precip"
-## SELECT
-vw.new.sel <- vw.new2 %>% select(Date, Year, Month, Day,
-                                 Max.Temp, Min.Temp, Mean.Temp,
-                                 Total.Precip)
-## drop empty rows after most recent date
-vw.new.sel.backup <- vw.new.sel
-vw.new.sel <- vw.new.sel %>% filter(Date<=Sys.Date())
-
-
-
-## apply season values
-seasons.mth <- read_csv('input/seasons.csv')
-vw.new.sel <- left_join(seasons.mth, vw.new.sel, by='Month')
-## set season yr: Dec falls into Winter of following yr
-vw.new.sel <- vw.new.sel %>% mutate(
-  Season.Yr=ifelse(Month==12,Year+1,Year))
-
-## add stn name
-vw.new.sel$station <- stn
-
-## SAVE yr_data ####
-write_csv(vw.new.sel, paste0("output/",stn_clean,"-weather-",yr_data,".csv"))
-
-## END NEW DATA IMPORT & CLEAN /////////////////////////////////////////////////
-
-## check dates
-summary(vw.new.sel$Date)
-vw_all_yrs <- vw.new.sel %>% filter(!is.na(Date)) ## legacy df name change for code below
-
-## SAVE all yrs data
-write_csv(vw_all_yrs, paste0("output/",stn_clean,"-weather-",min(vw_all_yrs$Year),"-",max(vw_all_yrs$Year),".csv"))
-
-## UPDATE EXISTING DATA WITH NEW ####
-## if first time collecting data for stn, save file
-## otherwise, append new data to existing file
-if(!file.exists(paste0("output/",stn_clean,"-weather.csv"))){
-  write_csv(vw_all_yrs, paste0("output/",stn_clean,"-weather.csv"))
-  vw.all <- vw_all_yrs
-} else {
-  ## Load existing data ####
-  vw.exist <- read_csv(paste0("output/",stn_clean,"-weather.csv"))
-  ## compare MAX date existing with MAX date new
-  vw_latest <- vw_all_yrs %>% filter(Date>max(vw.exist$Date))
-  ## ADD Latest to existing ####
-  vw.all <- bind_rows(vw.exist, vw_latest)
-  ## compare MIN data existing with MIN date new
-  vw_earliest <- vw_all_yrs %>% filter(Date<min(vw.exist$Date))
-  ## ADD earliest BEFORE existing (which is now vw.all)
-  vw.all <- bind_rows(vw_earliest, vw.all)
-}
-
+## APPEND NEW to OLD ####
+vw_all <- bind_rows(van_exist, vw.new_stn_clean)
 ## SAVE ####
-write_csv(vw.all, paste0("output/",stn_clean,"-weather.csv"))
-write_csv(vw.all, paste0("output/",stn_clean,max(vw.all$Date),"-weather.csv"))
-## save simple without station name in title
-write_csv(vw.all, "output/van-weather.csv")
-write_csv(vw.all, paste0("output/van-weather_",max(vw.all$Date),".csv"))
+write_csv(vw_all, 'output/van-weather.csv')
+write_csv(vw_all, paste0('output/van-weather_',max(vw_all$Date),'.csv'))
 
-## END UPDATE ###################################
-
-## SUMMARIZE ####
+## SUMMARIZE/CHECK ####
 ## check dates
-summary(vw.all$Date)
+summary(vw_all$Date)
 
 ## test for duplicated dates
-table(duplicated(vw.all$Date))
+table(duplicated(vw_all$Date))
+
+## check incomplete cases
+#vw_all[!complete.cases(vw_all),]
+
+
+
 
