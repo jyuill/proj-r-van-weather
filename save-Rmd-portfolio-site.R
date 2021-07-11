fe500ed195bd2ead23aee950dfe05637a838d342
John Yuill <john@fig4.com>
2021-07-10 18:45:04 -0700

    updates to van weather dashboard - essentially complete for what i am attempting. also added saving of latest weather csv to portfolio website to facilitate downloading from there.

--- save-Rmd-portfolio-site.R
+++ save-Rmd-portfolio-site.R
@@ -7,7 +7,12 @@ local <- here('van-weather-report.html')
 copy_to <- '/Users/jy/Documents/my-web/jyuill.github.io'
 file.copy(from=local, to=copy_to, overwrite = TRUE)
 
-## 
+## processed data
+local <- here('output','van-weather.csv')
+copy_to <- '/Users/jy/Documents/my-web/jyuill.github.io/data'
+file.copy(from=local, to=copy_to, overwrite = TRUE)
+
+## flexdashboard
 local <- here('van-weather-dashboard.html')
 copy_to <- '/Users/jy/Documents/my-web/jyuill.github.io'
 file.copy(from=local, to=copy_to, overwrite = TRUE)
