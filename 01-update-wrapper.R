a44cc6101dcd979a7c5bfe3622c7499c9b57fb59
John Yuill <john@fig4.com>
2021-08-14 18:26:10 -0700

    data updates; fix links to govt source data

--- 01-update-wrapper.R
+++ 01-update-wrapper.R
@@ -3,7 +3,8 @@
 ## get fresh data
 source('van-weather-import-v2.R')
 ## update main markdown analysis with new data
-knitr::knit2html('van-weather-report.Rmd')
+#knitr::knit2html('van-weather-report.Rmd') # doesn't look like this produces the right html format
+rmarkdown::render('van-weather-report.Rmd', output_format = "html_document") ## produces better result than above
 ## update flexdashboard with new data 
 rmarkdown::render("van-weather-dashboard.Rmd", output_format = flex_dashboard())
 ## copy examples over to portfolio website (on Macbook)
