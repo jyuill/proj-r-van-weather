15e470e71678f33ddb57f8853623e42a8e3046db
John Yuill <john@fig4.com>
2021-09-06 14:06:05 -0700

    weather updates

--- 01-update-wrapper.R
+++ 01-update-wrapper.R
@@ -6,6 +6,7 @@ source('van-weather-import-v2.R')
 #knitr::knit2html('van-weather-report.Rmd') # doesn't look like this produces the right html format
 rmarkdown::render('van-weather-report.Rmd', output_format = "html_document") ## produces better result than above
 ## update flexdashboard with new data 
-rmarkdown::render("van-weather-dashboard.Rmd", output_format = flex_dashboard())
+rmarkdown::render('van-weather-dashboard.Rmd')
+## doesn't work: rmarkdown::render("van-weather-dashboard.Rmd", output_format = flex_dashboard())
 ## copy examples over to portfolio website (on Macbook)
 source('save-Rmd-portfolio-site.R')
